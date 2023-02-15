163a3c425c0c95481821cbf30c367ce01791e76e
Mouadboukniter05 <mouad.boukniter05@gmail.com>
2023-02-15 23:21:22 +0100

    first project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,28 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'apptest';
+  public content = '';
+  public color = 'red';
+  public displayButton = true;
+
+  public users: Array<{ nom: string }> = [
+    {
+      nom: 'hamid',
+    },
+    {
+      nom: 'mouad',
+    },
+    {
+      nom: 'test',
+    },
+    {
+      nom: 'ls -h',
+    },
+    {
+      nom: 'cat file >>file1',
+    },
+  ];
 }
